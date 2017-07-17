0376c8217b899b2507328afe75644aac6c83a504
Pichest Wongsiripiphat <pichest.wongsiripiphat@thomsonreuters.com>
2017-07-17 13:02:20 +0700

    Add descriptor path argument and refactor accepting arguments

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import sys
 import os
-import getopt
+import argparse
 import subprocess
 import preparator
 from downloader import Downloader
 from recognizer import Recognizer
-
-train_rate = 0
 
 def do_cluster():
     FNULL = open(os.devnull, 'w') 
@@ -16,25 +14,23 @@ def do_cluster():
             args = "./cleaner/clean.exe \"" + os.path.abspath('./downloaded/' + dir_name) + "\" 0.45 \"" + os.path.abspath("./cleaned_data") + "\""
             subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=False)
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:],'hf:t:',['file=, trainRate='])
-    print opts
-except getopt.GetoptError:
-        sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
-        print "Help"
-    elif opt in ('-f', '--file'):
-        input_file = arg
-        print 'Reading file: ', input_file
-    elif opt in ('-t', '--trainRate'):
-        train_rate = int(float(arg))
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--file', '-f', help='name list input file path')
+parser.add_argument('--trainrate', '-t', help='specify training set rate', default = 80)
+parser.add_argument('--descriptor', '-d', help='specify descriptor path', default = os.path.abspath('./core/Descriptor'))
+parser.add_argument('--clean', help='clean descriptor before proceed', default = False, action='store_true')
 
-if train_rate is 0:
-    train_rate = 80
+args = parser.parse_args()
+input_file = os.path.abspath(args.file)
+train_rate = int(float(args.trainrate))
+descriptor_path = os.path.abspath(args.descriptor)
+descriptor_clean_required = args.clean
 
-print 'training set: ',  train_rate
-print 'test set: ',  100 - train_rate
+print 'Name List File Path: ' + input_file
+print 'Working Descriptor Path: ' + descriptor_path
+print 'Descriptor Clean Required: ' + str(descriptor_clean_required)
+print 'Training Set Rate: ',  train_rate
+print 'Test Set Rate: ',  100 - train_rate
 
 with open(input_file) as f:
     content = f.readlines()
@@ -42,6 +38,11 @@ with open(input_file) as f:
 preparator.empty_directory('./prepared')
 preparator.empty_directory('./cleaned_data')
 preparator.empty_directory('./downloaded')
+
+if descriptor_clean_required is True:
+    preparator.empty_directory(descriptor_path)
+    preparator.empty_directory(descriptor_path + "/Faces")
+    preparator.empty_directory(descriptor_path + "/Svms")
 
 downloader = Downloader([x.strip() for x in content])
 downloader.recurring_retrieve()
